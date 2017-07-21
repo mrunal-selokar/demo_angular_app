0ab33eab59888c334c25952039c53fecfc90b364
mrunal-selokar <mrunal_selokar@outlook.com>
2017-07-21 14:32:46 +0530

    git commit -m "simple hobbies toggle button added "

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -5,18 +5,41 @@ import { Component } from '@angular/core';
   templateUrl: 'user.component.html'
 })
 export class UserComponent {
-  title = 'Welcome to the demo application!';
-  name = 'Mrunal Selokar';
-  email = 'mrunal_selokar@outlook.com';
-  address = {
-    plot: "26",
-    area : 'Sarwashree nagar',
-    street: 'Umred roas',
-    city: 'Nagpur',
-    zip: '440024'
-  }
+  title:string;
+  name:string;
+  email:string;
+  address:address;
+  hobbies: string[];
+  showHobbies: boolean;
   constructor(){
       console.log('Constructor ran!');
-  }
+        this.title = 'Welcome to the demo application!';
+        this.name = 'Mrunal Selokar';
+        this.email = 'mrunal_selokar@outlook.com';
+        this.address = {
+            plot: "26",
+            area : 'Sarwashree nagar',
+            street: 'Umred roas',
+            city: 'Nagpur',
+            zip: '440024'
+        }
+        this.hobbies = ["drawing","doodling","music"];
+        this.showHobbies = false;
+    }
 
+    toggleHobbies(){
+        if(this.showHobbies == true){
+            this.showHobbies = false;
+        }
+        else{
+            this.showHobbies = true;
+        }
+    }
+}
+interface address{
+    plot:string;
+    area:string;
+    street: string;
+    city: string;
+    zip:string;
 }
